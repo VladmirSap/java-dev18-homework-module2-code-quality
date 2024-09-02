abe62d0e189e28c976437be17ec9566e7839fe8c
VladmirSap <vladmirsap@gmail.com>
2024-09-02 14:27:52 +0300

    create new branch

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main (String[] args) {
+        Scanner scan = new Scanner (System.in);
         byte input;
         byte rand;
         byte i;
-        boolean boxAvailable = false;
+        boolean boxAvailable;
         byte winner = 0;
-        char box[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char[] box = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         System.out.println("Enter box number to select. Enjoy!\n");
 
         boolean boxEmpty = false;
@@ -19,19 +19,19 @@ public class App {
             System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
             System.out.println("-----------");
             System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
-            if(!boxEmpty){
-                for(i = 0; i < 9; i++)
+            if (!boxEmpty) {
+                for (i = 0; i < 9; i++)
                     box[i] = ' ';
                 boxEmpty = true;
             }
 
-            if(winner == 1){
+            if (winner == 1) {
                 System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
-            } else if(winner == 2){
+            } else if (winner == 2) {
                 System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
-            } else if(winner == 3){
+            } else if (winner == 3) {
                 System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
             }
@@ -50,22 +50,22 @@ public class App {
                     System.out.println("Invalid input. Enter again.");
             }
 
-            if((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
+            if ((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
                (box[0]=='X' && box[3]=='X' && box[6]=='X') || (box[1]=='X' && box[4]=='X' && box[7]=='X') || (box[2]=='X' && box[5]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')){
+               (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')) {
                    winner = 1;
                    continue;
             }
 
             boxAvailable = false;
-            for(i=0; i<9; i++){
-                if(box[i] != 'X' && box[i] != 'O'){
+            for (i=0; i<9; i++) {
+                if (box[i] != 'X' && box[i] != 'O') {
                     boxAvailable = true;
                     break;
                 }
             }
 
-            if(boxAvailable == false){
+            if (!boxAvailable) {
                 winner = 3;
                 continue;
             }
@@ -78,11 +78,11 @@ public class App {
                 }
             }
 
-            if((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
+            if ((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
                (box[0]=='O' && box[3]=='O' && box[6]=='O') || (box[1]=='O' && box[4]=='O' && box[7]=='O') || (box[2]=='O' && box[5]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')){
+               (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')) {
                 winner = 2;
-                continue;
+
             }
         }
 
